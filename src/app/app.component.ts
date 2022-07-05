29003f879d6884c8c4b878fbca12204f97a9852d
Apporvaarya <apporvaarya@gmail.com>
2022-07-05 20:01:19 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component, VERSION } from '@angular/core';
 
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-  userData:any={}
+  userData: any = {};
 
-  getData(data:any)
-  {
+  getData(data: any) {
     console.warn(data);
-    this.userData=data
+    this.userData = data;
+  }
+
+  display = true;
+
+  toggle(para: any) {
+    para.display = !para.display;
   }
 }
